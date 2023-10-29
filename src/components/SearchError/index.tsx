d603fda0a62dbe41e9afc990dada836dffb3ed7b
Max <makasyslar@gmail.com>
2023-10-29 12:50:47 +0200

    Fix path error for images

--- src/components/SearchError/index.tsx
+++ src/components/SearchError/index.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.scss";
 const SearchError = ({ error }: { error: string }) => {
 	return (
 		<div className={styles.root}>
-			<img className={styles.img} src="/images/error-pikachu.gif" alt="Pokemon-error" />
+			<img className={styles.img} src="./images/error-pikachu.gif" alt="Pokemon-error" />
 			<p className={styles.text}>Error: {error}</p>
 		</div>
 	);
