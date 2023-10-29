80b24e3ba15870bdcd688ccf28c38fd9a74f0586
Max <makasyslar@gmail.com>
2023-10-29 13:12:03 +0200

    Fix path error for images

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,9 +1,9 @@
 import styles from './styles.module.scss'
-
+import pikachu from '/images/pikachu.gif';
 const Loading = () => {
   return (
     <div className={styles.root}>
-        <img className={styles.img} src="../../assets/images/pikachu.gif" alt="Pikachu" />
+        <img className={styles.img} src={pikachu} alt="Pikachu" />
         <span className={styles.text}>Loading . . .</span>
     </div>
   )
--- src/components/SearchError/index.tsx
+++ src/components/SearchError/index.tsx
@@ -1,9 +1,10 @@
 import styles from "./styles.module.scss";
+import pikachuError from '/images/error-pikachu.gif';
 
 const SearchError = ({ error }: { error: string }) => {
 	return (
 		<div className={styles.root}>
-			<img className={styles.img} src="../../assets/images/error-pikachu.gif" alt="Pokemon-error" />
+			<img className={styles.img} src={pikachuError} alt="Pokemon-error" />
 			<p className={styles.text}>Error: {error}</p>
 		</div>
 	);
