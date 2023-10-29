450811594f8940217e296e5aeb1e5f33714fd9a2
Max <makasyslar@gmail.com>
2023-10-29 13:04:30 +0200

    Fix path error for images

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.scss'
 const Loading = () => {
   return (
     <div className={styles.root}>
-        <img className={styles.img} src="./images/pikachu.gif" alt="Pikachu" />
+        <img className={styles.img} src="../../assets/images/pikachu.gif" alt="Pikachu" />
         <span className={styles.text}>Loading . . .</span>
     </div>
   )
